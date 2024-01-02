99fd8a501370875316ecba62e1a14ee1cf402030
ZinKoLwin-terminator <zinkolwin161198@gmail.com>
2024-01-02 13:32:03 +0630

    project init

---
+++ src/components/PostList.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Post List</h1>
+ <div v-for="post in posts" :key="post.id">
+<SinglePost :post="post"></SinglePost>
+ </div>
+</template>
+
+<script>
+import { onUnmounted } from 'vue';
+import SinglePost from './SinglePost'
+export default {
+  components: { SinglePost },
+    props:['posts'],
+    setup(props) {
+     
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/SinglePost.vue
@@ -0,0 +1,23 @@
+<template>
+  <h2>{{ post.title }}</h2>
+<p>{{ cutPostBody }}</p>
+</template>
+
+<script>
+import { computed } from 'vue'
+export default {
+    props: ['post'],
+    setup(props) {
+        let cutPostBody = computed(() => {
+            return props.post.body.substring(0, 200)+"........";
+        })
+
+        return {cutPostBody}
+}
+
+}
+</script>
+
+<style>
+
+</style>
