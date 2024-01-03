d25cb8bfa03bf7b77939407693b7b5b800855545
ZinKoLwin-terminator <zinkolwin161198@gmail.com>
2024-01-03 16:30:19 +0630

    create post backend

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,5 +1,5 @@
 <template>
- <form>
+ <form @submit.prevent="addPost">
     <label>Title</label>
     <input type="text" required v-model="title">
 
@@ -28,8 +28,23 @@ export default {
         tags.value.push(tag.value)
       }
       tag.value=""
+        }
+        let addPost=async()=>{
+        await fetch("http://localhost:3000/posts",{
+          method:"POST",
+          headers:{
+            "Content-type":"application/json"
+          },
+          body:JSON.stringify(
+            {
+              title:title.value,//""
+              body:body.value,//""
+              tags:tags.value//[]
+            }
+          )
+        })
     }
-    return {title,body,tag,handleKeydown,tags}
+    return {title,body,tag,handleKeydown,tags,addPost}
   }
 }
 </script>
